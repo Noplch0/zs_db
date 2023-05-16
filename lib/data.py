2d6c328f40600277e7d76b120f2590f368b6f269
Noplch0 <a2579950433@outlook.com>
2023-05-16 21:05:58 +0800

    update1

--- lib/data.py
+++ lib/data.py
@@ -2,8 +2,6 @@ import requests
 
 
 def get_html(code, exchange):
-    url = "https://www.google.com/finance/quote/" + code + ":%S" % exchange
+    url = "https://www.google.com/finance/quote/" + code + ":%s" % exchange
     text = requests.get(url)
     return text
-
-
